be413bfe75842e82d8f70fdbe537cbac4e87a8bb
Tom Liu (Liu Hsiu-Chi) <0xtomlord1122@gmail.com>
2026-08-13 23:20:57 +0800

    refactor(comet-sdk): remove codex protocol facades
    Signed-off-by: Tom Liu (Liu Hsiu-Chi) <0xtomlord1122@gmail.com>

--- comet-sdk/provider/codex/stream.go
+++ comet-sdk/provider/codex/stream.go
@@ -10,16 +10,6 @@ import (
 	"github.com/cometline/comet-sdk/internal/responsesproto"
 )
 
-type codexStreamState = responsesproto.StreamState
-
-func toSDKEvents(eventType, data string, state *codexStreamState) ([]cometsdk.Event, error) {
-	return responsesproto.ToSDKEvents(providerID, eventType, data, state)
-}
-
 func parseLoop(ctx context.Context, providerID, modelID string, emitToolStart bool, body io.ReadCloser, ch chan<- cometsdk.Event, log *slog.Logger, idleTimeout time.Duration) {
 	responsesproto.ParseLoop(ctx, providerID, modelID, emitToolStart, body, ch, log, idleTimeout)
-}
-
-func redactEncryptedReasoning(data string) string {
-	return responsesproto.RedactEncryptedReasoning(data)
 }
